628783d4f60bb6a03f4706ba4d04df5efbce539a
Faisal <faisaltsurya@gmail.com>
2025-03-05 10:55:31 +0700

    update main.tf

--- Instance/main.tf
+++ Instance/main.tf
@@ -101,3 +101,24 @@ resource "aws_ec2_tag" "target_type" {
   key         = "TargetType"
   value       = "AWS_ACCOUNT"
 }
+
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.ec2_free_tier.public_ip
+}
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2_free_tier.id
+}
+
+output "private_key_path" {
+  description = "Path to the private key file"
+  value       = local_file.private_key.filename
+}
+
+output "elastic_ip" {
+  description = "Elastic IP Address of the instance"
+  value       = aws_eip.elastic_ip.public_ip
+}
