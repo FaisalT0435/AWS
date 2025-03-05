239aaebc0b1eb8647e600c6741812a0dba19e6a5
Faisal <faisaltsurya@gmail.com>
2025-03-05 09:37:52 +0700

    update EC2 Terraform last

--- output.tf
+++ output.tf
@@ -1,22 +1,23 @@
-output "developer_group_id" {
-  value = aws_identitystore_group.developer.group_id
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.ec2_free_tier.public_ip
 }
 
-output "admin_group_id" {
-  value = aws_identitystore_group.Admin.group_id
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ec2_free_tier.id
 }
 
-output "power_user_group_id" {
-  value = aws_identitystore_group.Power_User.group_id
+output "private_key_path" {
+  description = "Path to the private key file"
+  value       = local_file.private_key.filename
 }
 
-output "read_only_group_id" {
-  value = aws_identitystore_group.ReadOnly.group_id
+output "elastic_ip" {
+  description = "Elastic IP Address of the instance"
+  value       = aws_elastic_ip.elastic_ip.public_ip
 }
 
-output "root_group_id" {
-  value = aws_identitystore_group.Root.group_id
-}
 # Output any necessary outputs from the module
 # output "developer_group_id" {
 #   value = module.iam.developer_group_id
