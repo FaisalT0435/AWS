e2f8d44734a597490dc9fa76ad67a09b1e1832c6
Faisal <faisaltsurya@gmail.com>
2025-03-05 10:26:44 +0700

    delete output.tf

--- Instance/output.tf
+++ Instance/output.tf
@@ -1,19 +1,19 @@
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.ec2_free_tier.public_ip
-}
+# output "public_ip" {
+#   description = "Public IP of the EC2 instance"
+#   value       = aws_instance.ec2_free_tier.public_ip
+# }
 
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.ec2_free_tier.id
-}
+# output "instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.ec2_free_tier.id
+# }
 
-output "private_key_path" {
-  description = "Path to the private key file"
-  value       = local_file.private_key.filename
-}
+# output "private_key_path" {
+#   description = "Path to the private key file"
+#   value       = local_file.private_key.filename
+# }
 
-output "elastic_ip" {
-  description = "Elastic IP Address of the instance"
-  value       = aws_elastic_ip.elastic_ip.public_ip
-}
+# output "elastic_ip" {
+#   description = "Elastic IP Address of the instance"
+#   value       = aws_eip.elastic_ip.public_ip
+# }
